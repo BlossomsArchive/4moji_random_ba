6e5ac4bca35e115c84284bb9c3ff0814a14199dd
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-04 15:06:18 +0900

    Add files via upload

---
+++ 4moji.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+from misskey import Misskey
+
+import random
+import os
+
+misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+misskey_token = os.environ.get("MISSKEY_TOKEN")
+
+moji4 = Misskey(misskey_address)
+moji4.token = misskey_token
+
+# coding:utf-8
+hiragana = ['ぁ', 'あ', 'ぃ', 'い', 'ぅ', 'う', 'ぇ', 'え', 'ぉ', 'お', 'か', 'が', 'き', 'ぎ', 'く', 'ぐ', 'け', 'げ', 'こ', 'ご', 'さ', 'ざ', 'し', 'じ', 'す', 'ず', 'せ', 'ぜ', 'そ', 'ぞ', 'た', 'だ', 'ち', 'ぢ', 'っ', 'つ', 'づ', 'て', 'で', 'と', 'ど', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ば', 'ぱ', 'ひ', 'び', 'ぴ', 'ふ', 'ぶ', 'ぷ', 'へ', 'べ', 'ぺ', 'ほ', 'ぼ', 'ぽ', 'ま', 'み', 'む', 'め', 'も', 'ゃ', 'や', 'ゅ', 'ゆ', 'ょ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'ゎ', 'わ', 'ゐ', 'ゑ', 'を', 'ん','ー']
+
+
+a4= random.choice(hiragana)
+b4= random.choice(hiragana)
+c4= random.choice(hiragana)
+d4= random.choice(hiragana)
+
+moji4.notes_create(text=a4+b4+c4+d4)
