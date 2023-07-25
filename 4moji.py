1cc2662b9dc13752c02b9e9096d427739f78f4ef
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-25 20:07:02 +0900

    Update 4moji.py

--- 4moji.py
+++ 4moji.py
@@ -16,18 +16,18 @@ moji4.token = misskey_token
 hiragana = ['ぁ', 'あ', 'ぃ', 'い', 'ぅ', 'う', 'ぇ', 'え', 'ぉ', 'お', 'か', 'が', 'き', 'ぎ', 'く', 'ぐ', 'け', 'げ', 'こ', 'ご', 'さ', 'ざ', 'し', 'じ', 'す', 'ず', 'せ', 'ぜ', 'そ', 'ぞ', 'た', 'だ', 'ち', 'ぢ', 'っ', 'つ', 'づ', 'て', 'で', 'と', 'ど', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ば', 'ぱ', 'ひ', 'び', 'ぴ', 'ふ', 'ぶ', 'ぷ', 'へ', 'べ', 'ぺ', 'ほ', 'ぼ', 'ぽ', 'ま', 'み', 'む', 'め', 'も', 'ゃ', 'や', 'ゅ', 'ゆ', 'ょ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'ゎ', 'わ', 'ゐ', 'ゑ', 'を', 'ん','ー']
 
 try:
-  a4= random.choice(hiragana)
-  b4= random.choice(hiragana)
-  c4= random.choice(hiragana)
-  d4= random.choice(hiragana)
-  moji4.notes_create(text=a4+b4+c4+d4)
+    a4 = random.choice(hiragana)
+    b4 = random.choice(hiragana)
+    c4 = random.choice(hiragana)
+    d4 = random.choice(hiragana)
+    moji4.notes_create(text=a4 + b4 + c4 + d4)
 except:
-  time.sleep(30)
-  print('リトライ\n')
-  a4= random.choice(hiragana)
-  b4= random.choice(hiragana)
-  c4= random.choice(hiragana)
-  d4= random.choice(hiragana)
-  moji4.notes_create(text=a4+b4+c4+d4)
+    time.sleep(30)
+    print("リトライ\n")
+    a4 = random.choice(hiragana)
+    b4 = random.choice(hiragana)
+    c4 = random.choice(hiragana)
+    d4 = random.choice(hiragana)
+    moji4.notes_create(text=a4 + b4 + c4 + d4)
 
 print(a4+b4+c4+d4+'\n')
