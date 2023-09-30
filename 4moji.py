e1367efa2ec32f2d8ffccef2608b38120e073db8
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-09-30 15:04:20 +0900

    Update 4moji.py

--- 4moji.py
+++ 4moji.py
@@ -6,12 +6,6 @@ import random
 import os
 import time
 
-misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
-misskey_token = os.environ.get("MISSKEY_TOKEN")
-
-moji4 = Misskey(misskey_address)
-moji4.token = misskey_token
-
 # coding:utf-8
 hiragana = ['ぁ', 'あ', 'ぃ', 'い', 'ぅ', 'う', 'ぇ', 'え', 'ぉ', 'お', 'か', 'が', 'き', 'ぎ', 'く', 'ぐ', 'け', 'げ', 'こ', 'ご', 'さ', 'ざ', 'し', 'じ', 'す', 'ず', 'せ', 'ぜ', 'そ', 'ぞ', 'た', 'だ', 'ち', 'ぢ', 'っ', 'つ', 'づ', 'て', 'で', 'と', 'ど', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ば', 'ぱ', 'ひ', 'び', 'ぴ', 'ふ', 'ぶ', 'ぷ', 'へ', 'べ', 'ぺ', 'ほ', 'ぼ', 'ぽ', 'ま', 'み', 'む', 'め', 'も', 'ゃ', 'や', 'ゅ', 'ゆ', 'ょ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'ゎ', 'わ', 'ゐ', 'ゑ', 'を', 'ん','ー']
 
@@ -21,6 +15,10 @@ c4 = random.choice(hiragana)
 d4 = random.choice(hiragana)
 while True:
     try:
+        misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+        misskey_token = os.environ.get("MISSKEY_TOKEN")
+        moji4 = Misskey(misskey_address)
+        moji4.token = misskey_token
         moji4.notes_create(text=a4 + b4 + c4 + d4)
     except:
         time.sleep(300)
