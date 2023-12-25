22a02e56ee43f94846cc16a59e914da9d9cf6de6
瑞井 箕月 <6161.k.iga@gmail.com>
2023-12-26 00:21:43 +0900

    Add files via upload

--- 4moji.py
+++ 4moji.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from misskey import Misskey
-
+from atproto import Client, models
 import random
 import os
 import time
@@ -13,16 +13,27 @@ a4 = random.choice(hiragana)
 b4 = random.choice(hiragana)
 c4 = random.choice(hiragana)
 d4 = random.choice(hiragana)
+post_text = a4 + b4 + c4 + d4
 while True:
     try:
         misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
         misskey_token = os.environ.get("MISSKEY_TOKEN")
         moji4 = Misskey(misskey_address)
         moji4.token = misskey_token
-        moji4.notes_create(text=a4 + b4 + c4 + d4)
+        moji4.notes_create(text=post_text)
     except:
         time.sleep(300)
     else:
         break
-    
+
+while True:
+    try:
+        # Bluesky
+        bluesky = Client()
+        bluesky.login(str(os.environ.get("BLUESKY_MAIL_ADDRESS")),str(os.environ.get("BLUESKY_PASSWORD")))
+        bluesky.send_post(post_text)
+    except:
+        time.sleep(300)
+    else:
+        break
 print(a4+b4+c4+d4+'\n')
